e99d4bf3326ad0f971803bce2fb9cb882b75d5b6
wbzhui <wbzhui@gmail.com>
2016-12-13 09:37:07 -0800

    Update extract.c

--- extract.c
+++ extract.c
@@ -164,7 +164,7 @@ int main(int argc, char *argv[])
                     }
                 }
                 printf("\r%ld...", times++);
-
+                fflush(stdout);
         }
 
         fclose(infile);
